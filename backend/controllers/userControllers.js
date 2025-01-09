25e5e99648e0debe26cff7a50ef19d217c9149b1
Milan Sony <milansonyofficial@gmail.com>
2025-01-09 12:56:50 +0530

    working on user signup

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,8 +1,31 @@
-const userSignup = (req, res) => {
-    res.status(200).send({
-        status: 200,
-        message: "user signup"
-    })
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+
+        // check empty input fields
+        if (!name) {
+            return res.status(400).send({
+                status: 400,
+                message: "Name field is required"
+            })
+        }
+        if (!email) {
+            return res.status(400).send({
+                status: 400,
+                message: "Email field is required"
+            })
+        }
+        if (!password) {
+            return res.status(400).send({
+                status: 400,
+                message: "Password field is required"
+            })
+        }
+
+        // check password length (<6)
+    } catch (error) {
+
+    }
 }
 
-export default userSignup
+export default signup
--- backend/routes/api/userRoutes.js
+++ backend/routes/api/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import userSignup from "../../controllers/userControllers.js";
+import signup from "../../controllers/userControllers.js";
 
 const userRoutes = express.Router()
 
 // user signup
-userRoutes.get("/signup", userSignup)
+userRoutes.post("/signup", signup)
 
 
 export default userRoutes
