16727887acdffb3a5403508790c0506cb4413350
Milan Sony <milansonyofficial@gmail.com>
2025-01-09 15:16:45 +0530

    feat: user model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
