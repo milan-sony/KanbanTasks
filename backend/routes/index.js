6574b914d600b39eab89b1c25816504101d48e1b
Milan Sony <milansonyofficial@gmail.com>
2025-01-09 10:34:39 +0530

    code formated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.use("/api/v1", api)
 
-const routes = router.use("/api", (req, res)=>{
+const routes = router.use("/api", (req, res) => {
     res.status(200).json({
         status: 200,
         message: "API works properly"
