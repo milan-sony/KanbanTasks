718be2a6a7266026678828fcfd6f67b7d03f5171
Milan Sony <milansonyofficial@gmail.com>
2025-01-10 17:20:27 +0530

    refactor: code refactured

--- backend/utils/generateTokens.js
+++ backend/utils/generateTokens.js
@@ -2,19 +2,24 @@ import jwt from "jsonwebtoken"
 
 const generateToken = (userId, res) => {
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    })
+    try {
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
 
-    // generated token is set as a cookie
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // Sets the lifetime of the cookie to 7 days (in milliseconds)
-        httpOnly: true, // prevent XSS
-        sameSite: "strict", // prevent CSRF
-        secure: process.env.NODE_ENV !== "development" // This flag ensures that the cookie is only sent over HTTPS connections. It is set to true in production (when NODE_ENV is not "development").
-    })
+        // generated token is set as a cookie
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // Sets the lifetime of the cookie to 7 days (in milliseconds)
+            httpOnly: true, // prevent XSS
+            sameSite: "strict", // prevent CSRF
+            secure: process.env.NODE_ENV !== "development" // This flag ensures that the cookie is only sent over HTTPS connections. It is set to true in production (when NODE_ENV is not "development").
+        })
 
-    return token
+        return token
+
+    } catch (error) {
+        console.error("Error generating token: ", error.message)
+    }
 }
 
 export default generateToken
