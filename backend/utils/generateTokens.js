875494ac284a454304f9b47be23e105c9c8d3d4e
Milan Sony <milansonyofficial@gmail.com>
2025-01-10 16:31:33 +0530

    feat: generate token utils added

---
+++ backend/utils/generateTokens.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const generateToken = (userId, res) => {
+
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // milliseconds
+        httpOnly: true, // prevent XSS
+        sameSite: "strict", // prevent CSRF
+        secure: process.env.NODE_ENV !== "development" // check http/https
+    })
+
+    return token
+}
+
+export default generateToken
