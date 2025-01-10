3ab01a3496ac9246128cd9f623c7e36b8200812b
Milan Sony <milansonyofficial@gmail.com>
2025-01-10 12:22:51 +0530

    a functionality to generate otp created

---
+++ backend/utils/otpManager.js
@@ -0,0 +1,40 @@
+import cryptoRandomString from "crypto-random-string"
+import Otp from "../models/otpModel.js"
+import mailSender from "./mailSender.js"
+
+// generate OTP
+function generateOTP() {
+    return cryptoRandomString({
+        length: 6,
+        type: "numeric"
+    })
+}
+
+// send otp via email
+const sendOTP = async (email) => {
+    try {
+        const OTP = generateOTP()
+        console.log("\n✔️  OTP generated")
+        const newOTP = new Otp({
+            email: email,
+            otp: OTP
+        })
+        if (!newOTP) {
+            console.error("Something went wrong, OTP not created")
+        }
+        await newOTP.save()
+
+        // send OTP via email
+        await mailSender({
+            to: email,
+            subject: "📢 Verify account - KanbanTasks",
+            message: `<p>Your OTP to verify KanbanTasks account is: <strong>${OTP}</strong></p>`
+        })
+        console.log(`\n✔️  OTP sent successfully to ${email}`)
+    } catch (error) {
+        console.error("An error occured at sending OTP via mail: ", error.message)
+    }
+}
+
+export default sendOTP
+
