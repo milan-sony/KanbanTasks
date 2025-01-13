dedc282aa1132fc6a89747cb52b4e2a247fa504a
Milan Sony <milansonyofficial@gmail.com>
2025-01-13 10:29:22 +0530

    feat: homepage created

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -3,12 +3,12 @@ import scrumBoard from "/undraw_scrum-board_uqku.svg"
 
 function Homepage() {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 h-64 bg-white dark:bg-black'>
-            <div className="p-10 flex flex-col justify-center">
+        <div className='grid grid-cols-1 md:grid-cols-2 h-64 bg-white dark:bg-black p-10'>
+            <div className="flex flex-col justify-center">
                 <h1 className='text-6xl font-black font-Open-Sans text-black dark:text-white pb-5'>KanbanTasks</h1>
                 <p className='text-xl font-Open-Sans font-medium text-gray-400'>Boost productivity and collaboration with our Kanban board for task assignment and integrated reminders.</p>
             </div>
-            <div className="p-10 flex justify-center items-center"><img src={scrumBoard} alt="image" className='size-[450px]' /></div>
+            <div className="flex justify-center items-center"><img src={scrumBoard} alt="image" className='size-[450px]' /></div>
         </div>
     )
 }
