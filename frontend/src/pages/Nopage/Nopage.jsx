8478e33b91bf954b38ba6e1985ca8d7019dbe801
Milan Sony <milansonyofficial@gmail.com>
2025-01-13 17:03:39 +0530

    style: 404 page modified

--- frontend/src/pages/Nopage/Nopage.jsx
+++ frontend/src/pages/Nopage/Nopage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Nopage() {
     return (
-        <div>
-            404 - page not found
+        <div className='flex flex-col justify-center h-screen w-1/2 p-20'>
+            <h1 className='text-black dark:text-white text-8xl font-black mb-4 font-Open-Sans'>404</h1>
+            <p className='text-gray-400 font-Open-Sans text-xl mb-4'>Page not found!</p>
+            <h1 className='text-gray-400 text-4xl font-bold font-Open-Sans'>KanbanTasks</h1>
         </div>
     )
 }
