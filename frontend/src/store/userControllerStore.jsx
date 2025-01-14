176e43f1acd861f7f43069ef6543dde32898ffb9
Milan Sony <milansonyofficial@gmail.com>
2025-01-14 17:13:05 +0530

    feat: user controller store created

---
+++ frontend/src/store/userControllerStore.jsx
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+import { axiosInstance } from '../lib/Axios'
+import { set } from 'mongoose'
+
+export const userControllerStore = create((set) => ({
+    authUser: null, //check whether user authenticated or not
+    isCheckingAuth: true, //state to check the authentication (initially set the value to true bcz asoonas we refresh the pg we will check whether the user is authenticated, with this the authUser value change)
+    isSigningUp: false,
+    isLogingIn: false,
+
+    // check user is authenticated on every change/routes
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/user/check")
+            set({ authUser: res.data })
+        } catch (error) {
+            set({ authUser: null })
+            console.error("Error in checking auth, ", error.message)
+        } finally {
+            set({ isCheckingAuth: false })
+        }
+    },
+}))
