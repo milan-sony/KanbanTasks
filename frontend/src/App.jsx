a1180f1d85999c2e18c24332e10b3dac1d51ad25
Milan Sony <milansonyofficial@gmail.com>
2025-01-15 12:05:42 +0530

    feat: toast configured, preloader added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { Toaster } from 'react-hot-toast'
 import Router from './router/Router'
+import { userControllerStore } from './store/userControllerStore'
+import { useEffect } from 'react'
+import { Loader } from 'lucide-react'
 
 function App() {
+
+  const { checkAuth, authUser, isCheckingAuth } = userControllerStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if (isCheckingAuth && !authUser) return (
+    <div className='h-screen flex justify-center items-center'>
+      <Loader className='size-10 animate-spin' />
+    </div>
+  )
+
   return (
     <>
       <Router />
+      <Toaster />
     </>
   )
 }
