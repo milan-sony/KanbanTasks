afab4db58642da0090e6777e8b9f3ca9db6d0e95
Milan Sony <milansonyofficial@gmail.com>
2025-01-28 16:45:14 +0530

    fix: redirect issue

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -5,7 +5,7 @@ import Signuppage from '../pages/Signuppage/Signuppage';
 import Loginpage from '../pages/Loginpage/Loginpage';
 import Nopage from '../pages/Nopage/Nopage';
 import LeanMorePage from '../pages/Learnmorepage/LeanMorePage';
-import VerifyOTPPage from '../pages/VerifyOTPPage/VerifyOTPPage';
+// import VerifyOTPPage from '../pages/VerifyOTPPage/VerifyOTPPage';
 import { userControllerStore } from '../store/userControllerStore';
 import Homepage from '../pages/Homepage/Homepage';
 
@@ -13,15 +13,27 @@ function Router() {
 
     const { authUser, isOTPVerified } = userControllerStore()
 
+    console.log("Router - authUser: ", authUser)
+    // console.log("Router - isOTPVerified: ", isOTPVerified)
+
     return (
+        // <Routes>
+        //     <Route path="/" element={<Indexpage />} />
+        //     <Route path="/learnmore" element={<LeanMorePage />} />
+        //     <Route path="/signup" element={!authUser ? <Signuppage /> : <Navigate to={"/verifyotp"} />} />
+        //     <Route path="/verifyotp" element={authUser && !isOTPVerified ? <VerifyOTPPage /> : <Navigate to={"/login"} />} />
+        //     <Route path="/login" element={!authUser ? <Loginpage /> : <Navigate to={"/home"} />} />
+        //     <Route path="/home" element={authUser ? <Homepage /> : <Navigate to={"/login"} />} />
+        //     <Route path="*" element={<Nopage />} />
+        // </Routes>
+
         <Routes>
-            <Route path="/" element={<Indexpage />} />
-            <Route path="/learnmore" element={<LeanMorePage />} />
-            <Route path="/signup" element={!authUser ? <Signuppage /> : <Navigate to={"/verifyotp"} />} />
-            <Route path="/login" element={!authUser ? <Loginpage /> : <Navigate to={"/home"} />} />
-            <Route path="/verifyotp" element={authUser || !isOTPVerified ? <Navigate to={"/home"} /> : <VerifyOTPPage />} />
-            <Route path="/home" element={authUser ? <Homepage /> : <Navigate to={"/login"} />} />
-            <Route path="*" element={<Nopage />} />
+            <Route path='/' element={<Indexpage />} />
+            <Route path='/learnmore' element={<LeanMorePage />} />
+            <Route path='/signup' element={!authUser ? <Signuppage /> : <Navigate to={"/home"} />} />
+            <Route path='/login' element={!authUser ? <Loginpage /> : <Navigate to={"/home"} />} />
+            <Route path='/home' element={authUser ? <Homepage /> : <Navigate to={"/login"} />} />
+            <Route path='*' element={<Nopage />} />
         </Routes>
     )
 }
--- frontend/src/store/userControllerStore.jsx
+++ frontend/src/store/userControllerStore.jsx
@@ -6,50 +6,51 @@ export const userControllerStore = create((set) => ({
     authUser: null, //check whether user authenticated or not
     isCheckingAuth: true, //state to check the authentication (initially set the value to true bcz asoonas we refresh the pg we will check whether the user is authenticated, with this the authUser value change)
     isSigningUp: false,
-    isLogingIn: false,
-    isOTPVerified: false,
+    // isLogingIn: false,
+    // isOTPVerified: false,
 
     // check user is authenticated on every change/routes
     checkAuth: async () => {
         try {
             const res = await axiosInstance.get("/user/check")
-            set({ authUser: res.data })
+            if (res) {
+                set({ authUser: res.data || null })
+            }
         } catch (error) {
             set({ authUser: null })
-            console.error("Error in checking auth, ", error.response.data.message)
+            console.error("Error in checking auth, ", error.response?.data?.message || error.message)
         } finally {
             set({ isCheckingAuth: false })
         }
     },
-
     // user signup
     signup: async (data) => {
-        set({ isSigningUp: true })
         try {
+            set({ isSigningUp: true })
             const res = await axiosInstance.post("/user/signup", data)
-            set({ authUser: res.data })
-            console.log("Data: ", data)
-            console.log("Response data: ", res.data)
-            return toast.success("Account created successfully")
+            if (res) {
+                set({ authUser: res.data })
+                return toast.success("Account created successfully")
+            }
         } catch (error) {
-            console.error("Error signing up user, ", error.response.data.message)
-            return toast.error("Error signing up user")
+            console.error("Error signing up user, ", error.response?.data?.message || error.message)
+            return toast.error(error.response?.data?.message || error.message || "Error in signing up user")
         } finally {
             set({ isSigningUp: false })
         }
     },
 
     // verify OTP
-    verifyOTP: async (data) => {
-        set({ isOTPVerified: true })
-        try {
-            await axiosInstance.post("/user/verifyotp", data)
-            return toast.success("OTP verified successfully")
-        } catch (error) {
-            console.error("Error verifying OTP, ", error.response.data.message)
-            return toast.error("Error verifying OTP")
-        } finally {
-            set({ isOTPVerified: false })
-        }
-    }
+    // verifyOTP: async (data) => {
+    //     try {
+    //         const res = await axiosInstance.post("/user/verifyotp", data)
+    //         if (res) {
+    //             set({ isOTPVerified: true })
+    //             return toast.success("OTP verified successfully")
+    //         }
+    //     } catch (error) {
+    //         console.error("Error verifying OTP, ", error.response.data.message)
+    //         return toast.error("Error verifying OTP")
+    //     }
+    // }
 }))
