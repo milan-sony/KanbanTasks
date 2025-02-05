3853cd2c79233277a4e1dde8aa0388af875ed82f
Milan Sony <milansonyofficial@gmail.com>
2025-02-05 11:49:34 +0530

    chore: otp verification removed

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import VerifyOTPPage from '../VerifyOTPPage/VerifyOTPPage'
+// import VerifyOTPPage from '../VerifyOTPPage/VerifyOTPPage'
 import { userControllerStore } from '../../store/userControllerStore'
 
 function Homepage() {
-    const { authUser, OTPVerified } = userControllerStore()
+    const { authUser } = userControllerStore()
 
     return (
         <div>
-            <Navbar />
             {
-                authUser && !OTPVerified ?
-                    <VerifyOTPPage />
-                    :
-                    <h1>heheh</h1>
+                !authUser ?
+                    <div className='flex flex-col justify-center h-screen w-1/2 p-20'>
+                        <h1 className='text-black dark:text-white text-8xl font-black mb-4 font-Open-Sans'>401</h1>
+                        <p className='text-gray-400 font-Open-Sans text-xl mb-4'>Unauthorized !</p>
+                        <h1 className='text-gray-400 text-4xl font-bold font-Open-Sans mb-4'>KanbanTasks</h1>
+                        <Link to={"/"} className='bg-black w-32 h-10 text-white font-Open-Sans font-medium rounded-md hover:bg-gray-600 dark:bg-white dark:text-black dark:hover:text-white mb-4 sm:mr-4 capitalize flex justify-center items-center'>Go back</Link>
+                    </div> :
+                    <>
+                        <Navbar />
+                        <h1 className='text-black dark:text-white'>Home Page</h1>
+                    </>
             }
+
         </div>
     )
 }
